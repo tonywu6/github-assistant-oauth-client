f2a92b53ab3cf40d4bda43e5fa436353b1a1b0a5
Tony Wu <tonywu6@protonmail.com>
2023-06-06 08:05:04 +0800

    use octokit/auth-oauth-user

--- api/auth.ts
+++ api/auth.ts
@@ -1,28 +1,20 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
-import fetch from 'node-fetch'
+import { createOAuthUserAuth } from '@octokit/auth-oauth-user'
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+export default async function (req: VercelRequest, res: VercelResponse) {
   const { code } = req.query
   if (!code) {
     return res.status(400).json({ message: 'Missing code' })
   }
-  const endpoint = new URL('https://github.com/login/oauth/access_token')
-  const query = new URLSearchParams({
-    client_id: process.env.GITHUB_CLIENT_ID,
-    client_secret: process.env.GITHUB_CLIENT_SECRET,
-    // repository_id: process.env.GITHUB_REPOSITORY_ID,
+  const auth = createOAuthUserAuth({
+    clientId: process.env.GITHUB_CLIENT_ID!,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET!,
     code: code as string,
   })
-  endpoint.search = query.toString()
-  const { access_token } = await fetch(endpoint, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-    },
-  }).then(async (r) => {
-    const data = (await r.json()) as Promise<{ access_token: string }>
-    console.log(data)
-    return data
-  })
-  return res.json({ access_token })
+  try {
+    const { token } = await auth()
+    return res.json({ token })
+  } catch (e) {
+    return res.status(401).json({ message: String(e) })
+  }
 }
